b9bb2a469d2ef2695205ed6145c68bab6e252f30
Joseph M <jmedina1985@hotmail.com>
2020-09-27 18:30:22 -0700

    Changed folder structure and included page transition animation

---
+++ app.js
@@ -0,0 +1,48 @@
+function pageTransition() {
+  const tl = gsap.timeline();
+
+  tl.to("ul.transition li", {
+    duration: 0.6,
+    scaleY: 1,
+    transformOrigin: "bottom left",
+    stagger: 0.1,
+  });
+  tl.to("ul.transition li", {
+    duration: 0.3,
+    scaleY: 0,
+    transformOrigin: "bottom left",
+    stagger: 0.2,
+    delay: 0.1,
+  });
+}
+
+function pictureAnimation() {}
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+        pageTransition();
+        await delay(1500);
+        done();
+      },
+      async enter(data) {
+        pictureAnimation();
+      },
+      async once(data) {
+        pictureAnimation();
+      },
+    },
+  ],
+});
